8b242a40ae820f38d84d1d9f4f2062283affa6e5
Antony Chan <antonyc@yahoo-inc.com>
2016-02-02 09:06:40 -0800

    stuff2

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -597,7 +597,7 @@ ngx_rtmp_hls_write_playlist(ngx_rtmp_session_t *s)
 
         if (hacf->keys && (i == 0 || f->key_id != prev_key_id)) {
             p = ngx_slprintf(p, end, "#EXT-X-KEY:METHOD=AES-128,"
-                             "URI=\"%V%V%s%s%uL.key\",IV=0x%032XL\n",
+                             "URI=\"%V%V%s/%s%uL.key\",IV=0x%032XL\n",
                              &hacf->key_url, &key_name_part, base64Buffer,
                              key_sep, f->key_id, f->key_id);
         }
